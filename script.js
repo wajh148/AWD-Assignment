b29517487052aba73ec73687bf311dd9980bb099
wajh148 <marukhimran37@gmail.com>
2025-10-31 11:32:47 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+const themeToggle = document.getElementById('theme-toggle');
+let isDarkMode = true;
+
+themeToggle.addEventListener('click', function() {
+    isDarkMode = !isDarkMode;
+    
+    if (isDarkMode) {
+        document.body.classList.remove('light-theme');
+        themeToggle.textContent = 'Light Mode';
+    } else {
+        document.body.classList.add('light-theme');
+        themeToggle.textContent = 'Dark Mode';
+    }
+});
+
